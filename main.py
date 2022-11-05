e768899750271413a7db449b35803eed8da3cc2f
AAAAA <AAAAA@DESKTOP-M0J6QD5.local>
2022-11-05 22:41:09 +0100

    Final commit

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def calc():
     x = int(request.args.get('x'))
     y = int(request.args.get('y'))
 
+    Inputs = Enum('Inputs', ['addition', 'subtraction', 'multiplication'])
     inputs = ['addition', 'subtraction', 'multiplication']
     key_words = ['add', 'subtract', 'multiply']
     operator_list = ['+', '-', 'x']
@@ -32,10 +33,10 @@ def calc():
         data = response['choices'][0]['text']
         for operator in operator_list:
             if operator in data:
-                operation = inputs[operator_list.index(operator)]
+                operation = Inputs(operator_list.index(operator) + 1).name
         for word in key_words:
             if word in user_input:
-                operation = inputs[key_words.index(word)]
+                operation = Inputs(key_words.index(word) + 1).name
                 break
         try:
             answer = int(data.split(' ')[-1].strip('.'))
@@ -43,11 +44,11 @@ def calc():
             answer = 'Invalid input'
     else:
         operation = user_input
-        if operation == inputs[0]:
+        if operation == Inputs(1).name:
             answer = x + y
-        elif operation == inputs[1]:
+        elif operation == Inputs(2).name:
             answer = x - y
-        elif operation == inputs[2]:
+        elif operation == Inputs(3).name:
             answer = x * y
     return jsonify(slackUsername='Fola27', result=answer, operation_type=operation)
 
